0aadddd952d89f67b4c51ac2ead3943133475e61
Redwann <redwannmelsa@gmail.com>
2022-04-17 04:35:57 +0200

    implemented basic logic to allow opening to be played

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navbar from './components/Navbar'
 
 import "./css/App.css";
 
+var indexOfComputer = 0;
+var indexOfPlayer = 0;
+
 const App = () => {
 
   const [isScoreOpen, setIsScoreOpen] = useState(false);
@@ -20,10 +23,22 @@ const App = () => {
     });
   }
 
+  function makeNewMove() {
+    if (game.game_over() || game.in_draw())
+      return setIsScoreOpen(true); // exit if the game is over
+    const moveSet = [
+      'c5', 'd6', 'cxd4'
+    ]
+    safeGameMutate((game) => {
+      game.move(moveSet[indexOfComputer])
+      indexOfComputer += 1;
+    })
+  }
+
   function makeRandomMove() {
     const possibleMoves = game.moves();
     if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
-      return; // exit if the game is over
+      return setIsScoreOpen(true); // exit if the game is over
     const randomIndex = Math.floor(Math.random() * possibleMoves.length);
     safeGameMutate((game) => {
       game.move(possibleMoves[randomIndex]);
@@ -32,23 +47,33 @@ const App = () => {
 
   function onDrop(sourceSquare, targetSquare) {
     let move = null;
-    safeGameMutate((game) => {
-      move = game.move({
-        from: sourceSquare,
-        to: targetSquare,
-        promotion: "q", // always promote to a queen for example simplicity
+    const playerMoveSet = [
+      'e4', 'f3', 'd4', 'd4'
+    ]
+    console.log(playerMoveSet[indexOfPlayer])
+    console.log(targetSquare)
+    if (targetSquare == playerMoveSet[indexOfPlayer]) {
+      safeGameMutate((game) => {
+        move = game.move({
+          from: sourceSquare,
+          to: targetSquare,
+          promotion: "q", // always promote to a queen for example simplicity
+        });
       });
-    });
-    if (move === null) return false; // illegal move
-    setTimeout(makeRandomMove, 200);
-    return true;
+      if (move === null) return false; // illegal move
+      setTimeout(makeNewMove, 200);
+      indexOfPlayer++
+      return true;
+    } else {
+      console.log('wrong move')
+    }
   }
 
   return (
     <div className="App-header">
       <Navbar isScoreOpen={isScoreOpen} setIsScoreOpen={setIsScoreOpen} />
       <div className="board">
-       <Chessboard position={game.fen()} onPieceDrop={onDrop} />  
+       <Chessboard position={game.fen()} onPieceDrop={onDrop} />
       </div>
     </div>
   );
